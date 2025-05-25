d29681da1041561092860392a148226c00a4a6b4
Korkogor <korkogor@gmail.com>
2025-05-25 16:18:45 +0300

    WIP

--- PasswordGeneratorUI/MainWindow.xaml.cs
+++ PasswordGeneratorUI/MainWindow.xaml.cs
@@ -21,4 +21,37 @@ namespace PasswordGeneratorUI
             InitializeComponent();
         }
     }
+    public class Program
+    {
+        static int GetPasswordLength()
+        {
+           // Console.WriteLine("Введите длину пароля: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static string GeneratePassword(int length)
+        {
+            const string chars =
+                "abcdefghijklmnopqrstuvwxyz" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "0123456789!@#$%&*";
+
+            var random = new Random();
+            var result = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+                result.Append(chars[random.Next(chars.Length)]);
+            return result.ToString();
+        }
+
+        static void Main()
+        {
+            var length = GetPasswordLength();
+            var password = GeneratePassword(length);
+
+            //Console.WriteLine($"Ваш случайный пароль: {password}");
+            //Console.WriteLine("Нажмите Enter для выхода...");
+            //Console.ReadLine();
+        }
+    }
 }
